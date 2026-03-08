77111b1c41474ea82d228727a14f9ed6429c040d
billal <kerimreshid7@gmail.com>
2026-03-08 13:36:33 +0300

    I have finished practical studing in the concepts of lstrip,r strip and strip(most used).

--- python_by_Baraa/04_day4_string_cleaning.py
+++ python_by_Baraa/04_day4_string_cleaning.py
@@ -5,55 +5,28 @@
 
 # space cleaning: this  is common task in real data sets the task is simple removing uncessary space 
 
-name='  $ kerim'.lstrip().strip('$')    # it's some boring and biggner method
+name='  $ kerim'.lstrip().strip('$')    # it's some boring and biggner method. won't give the exact what we want.
 print(name)      # this is how can clean left side of the value.
-name='  $ kerim'.lstrip(' $')      # but this is advanced and easy way to remove every extra speces and Spe Char  s. with regardless of order when we pass argument(in 1 argument) like this ('$ ') or (' $') . but this function will accept only one argument
+name='  $ kerim#'.strip('# $')      # but this is advanced and easy way to remove every extra speces and Spe Char  s. with regardless of order when we pass argument(in 1 argument) like this ('$ ') or (' $') . but this function will accept only one argument
 print(name)  
 skill='front end,sql and data anlysis $####   '.rstrip('$# ')  # this is fantastic way to remove uncessary spaces and special characters 
 print(skill)
+
+
+
 
 dep='  # software engineering  '.strip().strip('#')     #(won't remove one extra space where before # ) this is how we can remove extra spaces and uncessary special characters.
 print(dep)
 dep='  # software engineering  '.strip().replace('#','')  # the out put is same code 1 python gonna do first change # by empty then remove right and left spces and it's easy we usually use this.
 print(dep)
-dep='  # software engineering  '.strip(' #')      # this will clean every thing and it's the modern way to clean. 
-print(dep)
+cle_dep='  # software engineering  '.strip(' #')      # this will clean every thing and it's the modern way to clean. 
+dep='  # software engineering  '
+
+is_clean=cle_dep==dep
+print(f'no of extra characters={len(dep)-len(cle_dep)}')
+print('is my data is clean?',is_clean)
 
 print(f'{{ohh cleaning is simple and very intersting with python}}')
-
-
-# Challenge 1 — Clean messy usernames
-# You receive usernames from a file. Many have spaces or unwanted symbols at the start or end.
-
-usernames = [
-"   Bilal   ",
-"###Ahmed",
-"Fatima###",
-"   ##Yusuf##   "
-]
-
-# Task
-# Create a new list where:
-# All leading and trailing spaces are removed.
-# All  characters from the left and right are removed.
-
-new_usernames=[]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
