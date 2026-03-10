d7865379a6fcab1eb105c3ba65c4e6fe7b0fec33
billal <kerimreshid7@gmail.com>
2026-03-10 15:37:07 +0300

    some additional tasks on day 5.

--- python_by_Baraa/day5_Py_No_Int & Float, Ty, Math.py
+++ python_by_Baraa/day5_Py_No_Int & Float, Ty, Math.py
@@ -1,5 +1,5 @@
  
-    #  pyhon numbers
+    #  python numbers
 #types are built in functions in python
 # 1 type to know the type of the value,2 int we can use to change the value in to integer fro ex. from string,
 # 3 float to change to float and 4 complex to change to complex. these  are python built in functions.
@@ -7,7 +7,9 @@ x=12
 print(type(x))
 salary="3400"
 print(int(salary)*1.10)     #here we are changing the str to int and apply opratore * to increase the salary by 10%.
-gpa='3.83'
+phone="092863339"
+print(int(phone))          # i got the no starting from 9 it doesn't include 0 i thing 0 is not valid value for int class.
+
 gpa='3.83'
 print(float(gpa))          # we will show float which is the data type of the value. then to see  the data type we can apply type(gpa).
 
@@ -22,6 +24,12 @@ print(complex(x))            # now we have changed the value of x in to complex 
 a=34
 b=20
 c=54
+d=38
+
+# d=d+7        # we are increasing d by 7  there is anothe simple way let me show you.
+print(d)
+d+=7 
+print(d)         # the same calculation to the above. but a litttle bit smart.
 
 total=a-b+c           # python do every thing in order that we put.
 print(total)
@@ -36,7 +44,7 @@ average=(a+b+c)/3         # since python will apply BODMAS first calclate values
 print("the average of the values is ",average)      
 # Here I appleid four basic oprators in different way.
 
-print(a//b)              # it got 1.7 but round to 1 so, // is used to don't get float value so, after calculation python gonna round to the previous whole number regardless of rounding rule which is(if>5 then round to previous  else>=0.5 then round to next).
+print(a//b)              # it got 1.7 but round to 1 so, // is used to get int value not decimal or fraction. so, after calculation python gonna round to the previous(floor rounding) whole number regardless of rounding rule which is(if>5 then round to previous  else>=0.5 then round to next).
 total_value=a**4          # (to apply orders like sqr,cube,the power of 4 or 7...)we will get a*a*a*a  this is how exaclty apply square roots in python.
 print("the power of 4 of 34 is ",total_value)
 
@@ -51,19 +59,13 @@ print(7%3.5)     # here we get reminder 0 but 7 is not even and this is another 
 print(2-17)  # which is -15 it's not distance bc dis can't be -ve 
 print('the distance covered is',abs(2-17))   # absolute no are  +ve any time.
 
+result=3.83436778
+print(round(result))     # I got 4 bc I didn't defined the decimal then python round it to ceil 
 
+print(round(result,2))    #I got 3.83 this is how can we get rounded int value what ever we want from python. by the way it's my fresh man result.
 
-# # floar and ceil
-# gpa=3.8345376
-# print(gpa.__floor__())
-
-
-
-
-
-
-
-
+rou_res=3.83436778.__round__(2)  # agian I got 3.83. this is anothe way to print the above code.and i realize we can't pass int round function with out integer.
+print(rou_res)
 
 
 
