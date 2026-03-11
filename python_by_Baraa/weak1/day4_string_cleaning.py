2e037843ed0c9f8418afa5cb3eb39968d5298dea
billal <kerimreshid7@gmail.com>
2026-03-11 13:45:11 +0300

    Today I have orgnized the files in professional way.

---
+++ python_by_Baraa/weak1/day4_string_cleaning.py
@@ -0,0 +1,147 @@
+
+# string cleaning
+# since 80% of company datasets are string which means texts and they have bad data quality 
+# before we stsrt do something on them we have to clean it to get effective result from the datas
+
+# space cleaning: this  is common task in real data sets the task is simple removing uncessary space 
+
+name='  $ kerim'.lstrip().lstrip('$')    # it's some boring and biggner method. won't give the exact what we want.
+print(name)      # this is how can clean left side of the value.
+
+name='  $ kerim#'      # but this is advanced and easy way to remove every extra speces and Spe Char  s. with regardless of order when we pass parameter  like this ('$ ') or (' $') . but this function will accept only one argument
+print(name.strip('# $')) 
+name='  $ kerim#'.strip('# $')    
+print(name)                     #the same result so this is another way print final result
+
+skill='front end,sql and data anlysis $####   '.rstrip('$# ')  # this is fantastic way to remove uncessary spaces and special characters 
+print(skill)
+
+
+
+
+dep='  # software engineering  '.strip().strip('#')     #(won't remove one extra space where before # ) this is how we can remove extra spaces and uncessary special characters.
+print(dep)
+dep='  # software engineering  '.strip().replace('#','')  # the out put is same code 1 python gonna do first change # by empty then remove right and left spces and it's easy we usually use this.
+print(dep)
+cle_dep='  # software engineering  '.strip(' #')      # this will clean every thing and it's the modern way to clean. 
+dep='  # software engineering  '
+
+is_clean=cle_dep==dep
+print(f'no of extra characters={len(dep)-len(cle_dep)}')
+print('is my data is clean?',is_clean)
+
+print(f'{{ohh cleaning is simple and very intersting with python}}')
+
+
+
+# case cleaning  lowwer and upper methods 
+#  we use these most of the time in searching tasks bc when 
+# users search some thing mostly with typo from the original value so to fix such type of probles use
+course='python'.upper()
+name='KERIM reshid'.upper()    # THESE two code will made the latters capital and if it's already capital it will ignore and displlay it self.
+user_srch='Kerim'
+
+
+act_data='keriM'   
+print(user_srch==act_data)     # we will get false since pyhton is case sensetive we won't get what we looking for.
+
+user_srch='% Kerim  '.lower()
+act_data='#$$ keriM %'.lower()
+print(user_srch==act_data)      #  stil it's false bc of extra space 
+
+
+user_srch='% 3Kerim  '.lower().strip('% 3')
+act_data='#$$ (keriM %'.lower().strip('(%#$ ')
+print(user_srch==act_data)        # since i applied the concept strip from left and righ side(every uncessary values removed once). so we'll get true.
+
+# advanced challenge
+# clean this messy data 
+# expected output: Kerim Reshid,Addis Ababa,Ethiopia
+
+user_srch='% ###3Kerim Reshid,@ddis @baba,Ethiopia   -'.strip('#-% 3').replace('@','A')
+print(user_srch)    # in the out put i will get kerim Reshid,Addis Ababa,Ethiopia 
+
+
+
+# search functions  now we are gonna see checking string value using search.
+text = "Data analysis with python".startswith('data')  
+print(text)    # (false bc of case) this searching is used to check whether our string values starts as we want or not and in the out we will see True or False
+
+stu_info='Kerim reshid,3rd year,SWE'
+print(stu_info.endswith("SWE"))          # True  since, no matter to use it in print function and also to use single and double qouts interchangeably.
+
+file='sample datas.csv'         
+print(file.endswith('.csv'))      # True agian and it's important to check file type. 
+
+stu_info='Kerim reshid,3rd year,SWE'
+print('3rd' in stu_info)                 # true bc python searchs in the string value it gets then return True.     
+
+city = "Addis Ababa is the capital of Ethiopia"
+print(city.find('Ababa'))     # now here we won't see bool values bc it's related with indexing so it counts starting from zero then return index no.
+ # and it's not about single letter it find the word Ababa when it get it return the first index no of this word.
+
+# challenge to find exactly the removeal part index no  and slice(extract phone no with out country code)
+phone='+241-979-73658'
+print(phone.find('-'))    # we got 4 it means - is in the 4 index(5th position since indexing starts from 0)  the let me apply slicing then.
+
+print(phone[5:])       # simply we can solve the problem like this.
+print(phone[phone.find('-')+1:])  # same result but it's alittle more professional and advanced.
+
+# # validation  methods
+
+text = "Kerim"
+print(text.isalpha())
+print(phone.isdecimal())
+print(city.isnumeric())     # we can check like this  we will get True or False.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python_by_Baraa/weak2/day7_logic_opra.py
@@ -0,0 +1,138 @@
+
+# first let's see what is control flow is 
+# control flow is a logic we wrtie to control how our code runs
+# lets classify code in to two 
+# 1 straight line code    it just python gonna understand line by line no itration,no loop so we can say the Code we have written up to 
+# day 6 was streight forward codes.
+# 2 control flow code is a code that is we can command python how we want to run it by writing the logic.
+# so from now on i think i will prctice more control flow codes.
+# conditioal statments,loops and functions are good examples of CF
+
+#  I will see one by one. I will gonna master python like Baraa.   so, now lets see logic and oprators
+# logic and oprators in this topic we gonna see values(true false),functions(bool,all,any,isinstance),comparision oprators(<,=,>...), membership like(in and not in) and identity oprators(is and is not).
+
+# values
+print(True)
+print(False)
+
+# functions(type,bool,all,any,isinstance)
+#type
+print(type(True))    # type used to know the type of the value as we saw last weak.
+
+# bool
+print(True)
+print(bool(123))
+print(bool("2y346"))      # the function bool is used to get true or false by passing parameter
+print(bool(754.73))     # in these 4 condithions wwe will get True bc we have known values
+
+print(False)
+print(bool())
+print(bool(0))   #special case i need to understand further 
+print(bool(None))          
+print(bool(""))        # but, In these 5 conditions we will get false bc we have unknown values.
+
+# all and any   these built functions are used to check miltiple values on
+# all to get True all values must be True
+# any if there is atleast one  True value the out put put also True
+name='kerim'
+email='ke77@gmail.com'
+phone=''                       # it's empty string and differ from None which is unknown or nothing or no value.
+print(any([name,email,phone]))   # oviously it's True bc  we have two true values that satisfy the condition to be true in function any.
+print(all([name,email,phone]))   # False bc there is one false value. to be True all values must be true.
+
+s_name='kerim'
+s_email='ke77@gmail.com'
+s_phone='0976863826' 
+
+print(all([s_name,s_email,s_phone]))   # true bc  the values here are true
+print(any([s_name,s_email,s_phone]))  # true bc 'any' get true if atleast one condition is true. 
+
+
+# isinstance
+print(isinstance(23.45,float))    # isinstance(buikt in function) first check the data type of value then return true or false, in htis case we'll get True.
+print('kerim'.endswith('m'))       # we can evaluate statments like this
+print("SWE".startswith('s'))      # false bc of the case (it's starts with S not s)
+
+
+# comparision oprators(<,=,>...) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
