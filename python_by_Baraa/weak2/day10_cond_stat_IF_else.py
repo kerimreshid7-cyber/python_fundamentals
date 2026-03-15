bc555e007744ff937c6c05f50aac8c9b2269674f
billal <kerimreshid7@gmail.com>
2026-03-15 14:49:47 +0300

    I am learning conditional statments in python which is most important since my ambition is to be BI analyst. and i am learning the logic bhind the sign and doing practical real life examples.

---
+++ python_by_Baraa/weak2/day10_cond_stat_IF_else.py
@@ -0,0 +1,126 @@
+
+#       single if statments(only one condition) if the condition is true then go and excute it if it's false just skip  and go to the end
+# Challenge 1: Positive Number
+# Write a program that checks if a number is positive.
+x=34 
+if x>0:
+    print('yes it is positive')
+
+h=-2
+if h>0:
+    print('yes it is positive')
+
+k=0
+if k>0:
+    print('yes it is positive')
+
+# we got only one out bc the two conditions are false then pyhton just skipped them that we didn't get out put.
+
+
+# Challenge 2: Adult Check
+# Write a program to check if someone is an adult.
+# Rule:
+# If age is 18 or more, print "You are an adult".
+
+age=int(input("enter your age"))
+if age>=18:
+    print("you are adult")
+#if we enter enter 18 and above with no limite the out put is you are adult 
+# and it's not satisfying bc if the user enter <18 then python give us nothing so this is where we actaully need else and also elif.
+
+
+# 1. Even or Odd (%)
+# Ask the user to enter a number.
+# Rules:
+# If the number is divisible by 2 → print "Even number"
+# Otherwise → print "Odd number"
+number=int(input("enter a number"))
+if number%2==0:
+    print('the number is even')
+else:
+    print("the number is odd")
+# it's nice but what if the user input 0 or with out string this is exactly we need more explanation using elif(else if)
+
+# Challenge 1 — Even, Odd,Zero,or invalid input
+num=int(input('enter a number'))
+if num==0:
+    print(num,'is nither')
+elif num%2==0:
+    print(num,'is even')
+elif num%2==1:
+    print(num,'is odd')
+else:
+    print("invalid input")
+
+
+
+# i just realized that if we have more than one conditional statment(with user input) in our code
+# the exucution will be one after another not once. first come come fist seved.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
