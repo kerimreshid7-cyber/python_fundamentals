13c21a0a605af7647c053b2f1013985b36a1b5a1
billal <kerimreshid7@gmail.com>
2026-03-17 14:38:49 +0300

    i get to day 12 and i am studing nested if with clean logic. trying some practical challenges including  error handling.

---
+++ python_by_Baraa/weak2/day12_nested_if.py
@@ -0,0 +1,148 @@
+
+# nested if these are if statments inside another if statments to check multiple conditions once then to 
+# give appropriate respond
+
+# to i got to do real life practical challenges. lets continue
+
+#Challenge 1: Login system
+#Task:
+# Ask for username and password
+# If username is "admin":
+# If password is "1234" → “Login successful”
+# Else → “Wrong password”
+# Else → “User not found”
+
+username = input("Enter your name: ")
+password = input("Enter password: ")
+
+if username == "admin":
+    if password == "1234":
+        print("Login successful")
+    else:
+        print("Wrong password")
+else:
+    print("User not found")
+
+
+# Challenge 2: Even/Odd + Positive/Negative
+# Task:
+# Ask for a number:
+# First check if it's valid (number)
+# Then:
+# If number ≥ 0:
+# If even → “Positive Even”
+# Else → “Positive Odd”
+# Else:
+# If even → “Negative Even”
+# Else → “Negative Odd”
+
+
+try:
+    num = int(input("Enter a number: "))
+
+    if num >= 0:
+        if num % 2 == 0:
+            print("Positive Even")
+        else:
+            print("Positive Odd")
+    else:
+        if num % 2 == 0:
+            print("Negative Even")
+        else:
+            print("Negative Odd")
+
+except:
+    print("Invalid input")     # i used error handling here i know it before.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
