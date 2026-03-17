e5d051c6612b7d92d60fd1cf84cb65d8df8ade4f
billal <kerimreshid7@gmail.com>
2026-03-17 14:02:23 +0300

    some revision and deep understanding of the logic hind the sign.

--- python_by_Baraa/weak2/day11_cond_stat_IF_elif_else.py
+++ python_by_Baraa/weak2/day11_cond_stat_IF_elif_else.py
@@ -22,6 +22,19 @@ else:
 
 # i think this is boring. for sure i will modified it, when i got some practical experiance on coding.
 
+# i have understand one thing new in if statment ti's nice the logic be the highest to lowest we call it clean logic.
+# so now let me do this proble again.
+stu_score=float(input('enter your score here'))
+if score<0 or score>100:
+    print('invalid input')
+elif score>=90:
+    print('congrates you got A')
+else:
+    print('invalid input')    #by the way i know about error handling from my back ground experiance but i just to follow the road map of the video.
+
+# these two out codes will excute one after another by order.first come first served.
+
+
 
 # if is mandatory,only one,must come first, and condition must be appeared. 
 # elif optional, multiple times,come after if, and condition must be appeared.
