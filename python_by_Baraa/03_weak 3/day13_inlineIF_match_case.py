83e8623601112d90ded5ec00acef4b085c42be65
billal <kerimreshid7@gmail.com>
2026-04-01 10:16:53 +0300

    As usuall revision to examine what i have understood on yester days session.

--- python_by_Baraa/03_weak 3/day13_inlineIF_match_case.py
+++ python_by_Baraa/03_weak 3/day13_inlineIF_match_case.py
@@ -4,7 +4,7 @@
 #                         2, there is no elif here (not permible)
 #                         3, else is not an option rather it's mandatory.
 #                         4,  merge the code with print
-
+# we use it for simple tasks that needed simple logic.
 
 # Challenge 1 — Even or Odd
 # Write one line that prints "Even" if 17 is even, otherwise "Odd".
@@ -12,15 +12,13 @@ no=17
 print("even" if no%2==0 else "Odd")
 # it's super easy now lets try what if the callenge need to use elif as we know in not allowed to use elif in ternary. lets go together.
 
-# Challenge 2 — Even or Odd
+# Challenge 2 — Even,odd or nither
 # Write one line that prints "Even" if 17 is even, "odd" if a number is odd otherwise "nither".
 
-print('even' if no%2==0 else "odd" if no%2==1 else "nither")
-
+print("invalid" if not isinstance(no,int)else "even" if no%2==1 else "odd" if no%2==1 else "nither")
 
 # match case: in python it works as if statment but it's simple and easy some times it's treate as simpler form of if statment.
 # challenge 3  change the contry names into abbrivation
-
 country="ethiopia"
 
 if country=="ethiopia":
@@ -41,12 +39,6 @@ match m_country:
     case _:
         print("unknown country")
     
-
-
-
-
-
-
 
 
 
