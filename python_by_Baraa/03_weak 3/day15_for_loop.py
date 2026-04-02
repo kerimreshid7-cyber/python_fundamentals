b559aed8edacf1f5d93ee5806efcdd19b4fd82ea
billal <kerimreshid7@gmail.com>
2026-04-02 12:05:12 +0300

    wow it's very interesting topic. i am about master it.

--- python_by_Baraa/03_weak 3/day15_for_loop.py
+++ python_by_Baraa/03_weak 3/day15_for_loop.py
@@ -11,39 +11,50 @@ print()  #just because you know what i mean to  create gab.
 # challenge 2 print left aligned pyramid of stars with 6 rows
 for i in range(1,7):
     print("*"*i)
-
+print()
 
 
 # so lets continue todays new ideas or topics.
 # loop control statments  we will see three of them
 # break,continue,and pass        so we are about to see the advancd for loop.
 
-# break statment  is used to stop the itration when the condition is true if the condition is false then print and again return to the first step so the logic is like that.
 names=['ker','ebr','abd','syd',"",'ayub','sam']
+
 for name in names:
     if name=="":
         print('empty value detected!')
-        break
+    print(name)   # we didn't tell python what it should do next so by default it'll continue the loop and it'll print the empty space also(blank line)and then the remaining two names.
+
+print()
+
+
+# break statment  is used to stop the itration when the condition is true if the condition is false then print and again return to the first step so the logic is like that.
+for name in names:
+    if name=="":
+        print('empty value detected!')
+        break  # we told python to stop the loop using break.
     print(name)
 print()
-# what if the break statment missed lets see what will gonna happen
-for name in names:
-    if name=="":
-        print('empty value detected!')
-    print(name)    # here i get all names in the out put but after empty value detected! the order is unexpected for me it should be i need to learn some thing new. 
-print()
+
 
 
 # continue is used to continue the loop  after even the 2nd condition is true it's opposite to even 
 for name in names:
     if name=='':
         print('empty value detected!')
-        continue
+        continue  # which mean if name=='' then print empty value detected! and then skip this step(empty str) and continue the loop. so we won't get the blank line bc of continue.
     print(name)
 
+print()
 
 
-
+# pass used to command python "do nothing for now" 
+tasks = ["Clean room", "Do homework", "Call friend"]
+for task in tasks:
+    if task=="Do homework":
+        pass   # we are instracting pyhton to do nothing if task=Do homework so it'll gonna jump this task. 
+    else:
+        print(task)
 
 
 
