45b77b0cd22f2c63ebf2d3a16ed20d72d8bc6032
billal <kerimreshid7@gmail.com>
2026-04-02 14:06:00 +0300

    i got confused. but i  am sure that i'll  fix it.

--- python_by_Baraa/03_weak 3/day15_for_loop.py
+++ python_by_Baraa/03_weak 3/day15_for_loop.py
@@ -28,7 +28,7 @@ for name in names:
 print()
 
 
-# break statment  is used to stop the itration when the condition is true if the condition is false then print and again return to the first step so the logic is like that.
+# 1 break statment  is used to stop the loop(itration) when the condition is true if the condition is false then print and again return to the first step so the logic is like that.
 for name in names:
     if name=="":
         print('empty value detected!')
@@ -38,7 +38,7 @@ print()
 
 
 
-# continue is used to continue the loop  after even the 2nd condition is true it's opposite to even 
+# 2 continue is used to skip one step(thathe condition become true) but continue the loop.(skip this but continue the loop.)
 for name in names:
     if name=='':
         print('empty value detected!')
@@ -46,32 +46,69 @@ for name in names:
     print(name)
 
 print()
+# we use continue to skip bad or empty data but to continue the loop.
 
 
-# pass used to command python "do nothing for now" 
+
+# 3 pass used to command python "do nothing for now" 
 tasks = ["Clean room", "Do homework", "Call friend"]
 for task in tasks:
     if task=="Do homework":
-        pass   # we are instracting pyhton to do nothing if task=Do homework so it'll gonna jump this task. 
+        pass   # we are instracting pyhton to do nothing if task=Do homework so it'll gonna jump this task.  pass with else may make you confused with continue so take care of that.
     else:
         print(task)
+  
+print()
+
+# another ex.
+sales=[234,324,63433,"",7363,837]
+for sale in sales:
+    if sales=="":
+        print("didn't find sale here")
+        pass    # i realized when we should use pass with out else. to create space holder then may be when we get convinient time we will fix it so using pass.
+    print(sale)
+
+print()
+
+# challenge 1
+# print only working days (skip weakends) out of days
+working_days=['mon','tue','sun','wed','fri','sat','thu']
+weakend=['sat','sun']
+
+for day in working_days:
+    if day in weakend:
+        print("there was no work on that day")
+        continue
+    print(day)
+
+print()
+
+
+# challenge 2
+# scan email to block un safe data from entering your system.
+emails = [
+    "samir21@gmail.com",
+    "hana.kebede@yahoo.com",
+    "abel.tech2024@outlook.com",
+    "drop table users;"      # one way the hacker may use to distroy our data base or access and steal important info. which is called sql injection.
+    "selamwork12@gmail.com"
+]
+
+# for email in emails:
+#     if ";" in email:
+#         print('here there is sql injection')
+#         break
+#     print(email)
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# for else
+for email in emails:
+    if ";" in email:
+        print("here there is sql injection")
+        
+else:
+    print("end")
 
 
 
