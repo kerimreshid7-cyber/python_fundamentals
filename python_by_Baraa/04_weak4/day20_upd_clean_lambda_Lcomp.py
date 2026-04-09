d5aacd1547e1ca1b5af6b932d90e413d9bcfd87b
billal <kerimreshid7@gmail.com>
2026-04-09 08:53:52 +0300

    revision on day 20. and about to start todays session.

--- python_by_Baraa/04_weak4/day20_upd_clean_lambda_Lcomp.py
+++ python_by_Baraa/04_weak4/day20_upd_clean_lambda_Lcomp.py
@@ -20,7 +20,6 @@ matrix[-2].sort(reverse=True)   # to order specific row in descending.
 numbers.reverse()      # or ::-1
 new_numbers=reversed(numbers)   # it won't remove the orignal list like sorted.
 
-
 no1=[1,1,7,5]
 no2= [7,3.83]
 sum_no=no1+no2   # or no2 is extended after no1
@@ -29,10 +28,12 @@ sum_no=no1.extend(no2)    # these two methods have the same result.
 
 # copy the list   it's important to put data if something happened and we need to get the orignal data and also to compare the analysed data.
 matrix_copy=matrix    # python won't make another list object it will store the objecte that is created for the original list.
+matrix_copy.extend(["there was match yesteday. But,i didn't see it.",'so i gotto see highlight.'])
+print(matrix_copy)      # when ever we add new item in copy the original list also will be affected.
+print(matrix)        # so to fix this what we have to do?
 
-
-
-
+matrix_copy_independent=matrix_copy.copy       # so now we can easily analyse the orignal or copy list. no more affecting the copy while anlysing the orignal one and vice versa.
+# i think it's clear and easy right? lets continue.
 
 
 
