3cf30645d311455061152f25608f879d40d22357
billal <kerimreshid7@gmail.com>
2026-04-13 13:36:06 +0300

    today i will cover function  which is the most important concept in programing.

---
+++ python_by_Baraa/04_weak4/day23_function.py
@@ -0,0 +1,101 @@
+
+# hi every one today i will cover the basics of function in pyhton. 
+
+# what is function?
+# function is a block of code that perform  a specific task.so we can reuse it what ever we want.
+
+# why we need function?
+# once we use function for some specific problem then if we face (have to fix) the problem then simply copy and paste.
+# and function allows modularity which mean  breaking down the complex and larg systems then we can easily do it step by step.
+
+
+# types of functions:  there are four types of functions 
+# 1 built in function:built by pyhton devs and offer directly with python the simplest one.
+# 2 standard library: the offer not default like print() but we can easily import  and use them ex. math and random
+# 3 external library: built by another community not pyhton devs so we have to install them import and then use them simply.    ex panda, matplotlip and numpy.
+# 4 user defined fun f(x): this what we can build from the scrutch then we can use what ever we want so today i will cover this one. 
+
+
+# golden rule: cheack if it is in built in fun then in standard libraries then check your team user defined fun f(x) and  after that go and build the function in your own.
+
+
+# how function works
+
+# 1 definition:it's just defining the function.
+print('wow pyhton')       # pyhton will excute this authomatically
+
+def greeting():           # oh the user is commanding me to create function named greeting i to have to do it.
+    print('wow function')  # python won't give us out put rather i craete object and put the value then when we call it it returns. 
+print(greeting)             # this is how we can exactly call a function.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
