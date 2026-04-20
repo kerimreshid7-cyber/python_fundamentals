a578966ec8bd1b92e0a77dd67ac52451fe3928a9
billal <kerimreshid7@gmail.com>
2026-04-20 10:18:50 +0300

    simple modification.

--- 02_python_by_Baraa/03_weak 3/day18_data_structure_list.py
+++ 02_python_by_Baraa/03_weak 3/day18_data_structure_list.py
@@ -13,145 +13,69 @@
 #  List → accepts everything
 # Lists are the most flexible container.
 
-data = [
-    10,
-    "hello",
-    3.14,
-    True,
-    [1,2,3],
-    (4,5),
-    {"name":"Bilal"},
-    {1,2,3}
-]
+# data = [
+#     10,
+#     "hello",
+#     3.14,
+#     True,
+#     [1,2,3],
+#     (4,5),
+#     {"name":"Bilal"},
+#     {1,2,3}
+# ]
 
-mixed=[1,7,True,'kerim',3.83,]    # this is how we can create list and in list we can store dfnt data types 
-# but how pyhton store it?  fist crate  object list then store each values uniqly by create another objects regarding to there daata type.
-print(type(mixed))
-print(mixed,'\n')
+# mixed=[1,7,True,'kerim',3.83,]    # this is how we can create list and in list we can store dfnt data types 
+# # but how pyhton store it?  fist crate  object list then store each values uniqly by create another objects regarding to there daata type.
+# print(type(mixed))
+# print(mixed,'\n')
 
-cource=list("pyhton")  #to change normal str in to list format
-print(cource,'\n')
+# cource=list("pyhton")  #to change normal str in to list format
+# print(cource,'\n')
 numbers=list(range(7))
-print(numbers)
-print()
-# nested list or matrix.
+# print(numbers)
+# print()
+ ##nested list or matrix.
 nested=[[1,2,7,'wow',3.83],
-       [74,'now','then','here after'],
-       [True,False,None,7]]
-print(nested)
+        [74,'now','then','here after'],
+        [True,False,None,7]]
+# print(nested)
+print(nested[:,0])
+# # accessing list
+# # indexing(access or read one single item from normal or matrix list)
+# print(numbers[3])    # bc the fun range also start from zero like indexes.
+# print(cource[-4])    # in indexing if we use -ve nos then it will start from -1.
 
-# accessing list
-# indexing(access or read one single item from normal or matrix list)
-print(numbers[3])    # bc the fun range also start from zero like indexes.
-print(cource[-4])    # in indexing if we use -ve nos then it will start from -1.
+# print(nested[0][0])  # whenever you want to read or access the first item in nested or matrix list.
+# print(nested[1])      # to get the second row.
+# print(nested[2][-1])    # to get no 7 that appear in the third row and fourth column.
 
-print(nested[0][0])  # whenever you want to read or access the first item in nested or matrix list.
-print(nested[1])      # to get the second row.
-print(nested[2][-1])    # to get no 7 that appear in the third row and fourth column.
+# # slicing (access or read multiple items from normal or matrix list)
+# # slicing normal list
+# print(numbers[:3])
+# print(cource[3:])
 
-# slicing (access or read multiple items from normal or matrix list)
-# slicing normal list
-print(numbers[:3])
-print(cource[3:])
-
-# slicing matrix list
-print(nested[1:3])   # to read specific rows in matrix
-col1 = [row[1] for row in nested]   # te read the second column in the matrix.
-print(col1)                # you have to practice more in this concept understanding how it works and  knowing is this possible way to access such type of data is enough in this stage. the other thing will be how much you dedicated to use AI to solve problems.
-
-
-# but the important question is how we can specifc ordered values for ex. to read now then from the matrix.
-print(nested[1][1:3])   # we did it i can access exacttly what i want.
-print(nested[0][2:5])
+# # slicing matrix list
+# print(nested[1:3])   # to read specific rows in matrix
+# col1 = [row[1] for row in nested]   # te read the second column in the matrix.
+# print(col1)                # you have to practice more in this concept understanding how it works and  knowing is this possible way to access such type of data is enough in this stage. the other thing will be how much you dedicated to use AI to solve problems.
 
 
-# packing and unpacking lists
-employee=['kerim reshid',23,'BI analyst',"ethiopia"]
-# name=employee[0]
-# age=employee[1]
-# roll=employee[2]
-# country=employee[3]  #we can  assign specifically for what we want this is called unpacking. But it's boring and there is another simple way.
+# # but the important question is how we can specifc ordered values for ex. to read now then from the matrix.
+# print(nested[1][1:3])   # we did it i can access exacttly what i want.
+# print(nested[0][2:5])
 
-name,age,roll,country=employee                # and this is how we can unpack easily in pro way.
-print(f"name:{name} age:{age} years old")     # why do we need unpacking?
-# bc if we want to apply the functions like aggregation,transformation and so on we hve to assign each values to their data type.
+
+# # packing and unpacking lists
+# employee=['kerim reshid',23,'BI analyst',"ethiopia"]
+# # name=employee[0]
+# # age=employee[1]
+# # roll=employee[2]
+# # country=employee[3]  #we can  assign specifically for what we want this is called unpacking. But it's boring and there is another simple way.
+
+# name,age,roll,country=employee                # and this is how we can unpack easily in pro way.
+# print(f"name:{name} age:{age} years old")     # why do we need unpacking?
+# # bc if we want to apply the functions like aggregation,transformation and so on we hve to assign each values to their data type.
   
-teacher=['amir',19,'BI analyst',"ethiopia"]
-name,*details,country=teacher     # using astrix we can store detail informations then access all once but the formatt will be in list.
-print(name,details)               # and this is how we can  pack in list.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# teacher=['amir',19,'BI analyst',"ethiopia"]
+# name,*details,country=teacher     # using astrix we can store detail informations then access all once but the formatt will be in list.
+# print(name,details)               # and this is how we can  pack in list.
---
+++ 03_py_external_labs_by_brocode/weak1/day1_numpy.py
@@ -0,0 +1,112 @@
+
+
+
+# list vs array
+matrix=[[1,3,4,7],
+        [7,3.83],
+        [1,1,0,"",None,False],
+        [1,1,7,5,90.98]]
+
+my_list=matrix*3   #this code will make the list to appear 3 times it self not multilpying the value.
+
+print(my_list)
+
+# this is where exaclty we need array so to use array in pyhton we have to install and import the external library called numpy.
+
+import numpy as np
+array_0d=np.array('ohh array')          #it's 0 dimentional array
+
+array_1d=np.array(['kerim',3.18,3,'wow'])    #it's 1 dimentional array
+
+array_2d=np.array([[3.83,3,7],
+                   [3.79,2,5],
+                   [3.65,2,3]])
+
+array_3d = np.array([
+    [
+        ['kerim',3.83,3,'wow'],
+        ['ebrahim',3.79,3,'wow'],
+        ['abduselam',3.7,2,'wow']
+    ],
+    [
+        ['chala',3.83,3,'mine'],
+        ['bruk',3.79,3,'yours'],
+        ['mame',3.7,2,'our']
+    ],
+    [
+        ['jbo',3.83,3,'theirs'],
+        ['wawa',3.79,3,'his'],
+        ['arebu',3.7,2,'her']
+    ]
+])
+
+
+print(array_0d.ndim)
+
+print(array_1d.ndim)
+
+print(array_2d.ndim)
+
+print(array_3d.ndim)
+
+print()
+# so now we can easily applie mathimathical oprations like addition and multiplication on array bc we have imoprted numpy. and python with out nump is slow how ever with numpy it's fast.
+
+pro_array_2d=array_2d*3
+print(pro_array_2d)
+
+# array indexing and slicing
+# indexing
+print(pro_array_2d[-1][1])
+print(array_3d[2][2][3])     # this is how we can acces specific value from  3d arary using chain indexing
+print()
+# slicing
+print(array_2d[::2])
+print(array_2d[::-1])
+print()
+print(array_2d[::-2])  # to print reverse and to jump step by two.    
+
+
+print(array_2d[:,1])
+print(array_2d[1:3,1:3])   # this how we can easily access the specific row and specific column using this power full library.
+print()
+print(array_2d[:,0:2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
