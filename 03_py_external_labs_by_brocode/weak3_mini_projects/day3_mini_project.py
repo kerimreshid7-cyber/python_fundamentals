381d2e54a2798f01b4dfc02f1c71c0849ecf4b81
billal <kerimreshid7@gmail.com>
2026-05-06 11:01:13 +0300

    Today i have done 1 mini project that shows which country contributed the best market share and which contry the weakest.

---
+++ 03_py_external_labs_by_brocode/weak3_mini_projects/day3_mini_project.py
@@ -0,0 +1,23 @@
+"""
+PROJECT 2 — Country Revenue Analysis
+Identify strongest and weakest markets.
+"""
+
+import pandas as pd
+
+df = pd.read_csv("03_py_external_labs_by_brocode/weak3_mini_projects/a_business_data_2800.csv")
+
+country_rev = df.groupby("country")["revenue"].sum().sort_values(ascending=False)
+
+print("\nTOP PERFORMING COUNTRIES\n")
+print(country_rev)
+
+print("\nBEST MARKET:", country_rev.idxmax())
+print("WEAKEST MARKET:", country_rev.idxmin())
+
+
+
+
+# Insights:
+# • Identify expansion opportunities
+# • Find underperforming regions
