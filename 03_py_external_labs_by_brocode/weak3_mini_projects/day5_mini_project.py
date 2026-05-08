b4c7dcffaf2207cfb07e360e33113b7eca3da609
billal <kerimreshid7@gmail.com>
2026-05-08 16:44:28 +0300

    Add day 5 mini project for marketing channel performance analysis.

---
+++ 03_py_external_labs_by_brocode/weak3_mini_projects/day5_mini_project.py
@@ -0,0 +1,21 @@
+
+
+# hello how are you doing today? I hope you are doing well. I am going to do mini project for day 5.
+
+"""
+PROJECT 3 — Marketing Channel Performance
+Determine which marketing channel generates most revenue.
+"""
+
+import pandas as pd
+
+df = pd.read_csv("03_py_external_labs_by_brocode/weak3_mini_projects/a_business_data_2800.csv")
+
+channel_perf = df.groupby("marketing_channel")["revenue"].agg(["sum","mean","count"])
+channel_perf = channel_perf.sort_values("sum", ascending=False)
+
+print("\nMARKETING PERFORMANCE REPORT\n")
+print(channel_perf)
+
+# Insights:
+# • Which channels deserve more budget
