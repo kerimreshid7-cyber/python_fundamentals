7c58cef339ebc92e07c6728d2bba95fa49d86e15
billal <kerimreshid7@gmail.com>
2026-05-10 13:38:04 +0300

    Revision on day 6.

--- 03_py_external_labs_by_brocode/weak3_mini_projects/day6_mini_project.py
+++ 03_py_external_labs_by_brocode/weak3_mini_projects/day6_mini_project.py
@@ -9,10 +9,11 @@ df = pd.read_csv("03_py_external_labs_by_brocode/weak3_mini_projects/b_football_
 
 df["conversion_rate"] = df["goals"] / df["shots"].replace(0,1)
 
-eff = df.groupby("team")["conversion_rate"].mean().sort_values(ascending=False)
+Efficiency = df.groupby("team")["conversion_rate"].mean().sort_values(ascending=False)
 
 print("\nTEAM SHOOTING EFFICIENCY\n")
-print(eff)
+print(Efficiency)
 
 # Insights:
 # • Most efficient attacking teams
+# • Teams with the best goal conversion rates
