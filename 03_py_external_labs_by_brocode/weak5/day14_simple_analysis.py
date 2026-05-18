2bffce919a5cc0e76f5446640a8b1418c65aed05
billal <kerimreshid7@gmail.com>
2026-05-18 15:23:33 +0300

    I have done simple analysis for today.

---
+++ 03_py_external_labs_by_brocode/weak5/day14_simple_analysis.py
@@ -0,0 +1,15 @@
+"""
+Average Basket Size
+How many items customers buy per order.
+"""
+
+import pandas as pd
+
+df = pd.read_csv("03_py_external_labs_by_brocode/weak3_mini_projects/a_business_data_2800.csv")
+
+basket = df.groupby("order_id")["quantity"].sum().mean()
+print("\nAverage Items Per Order:", basket)
+
+
+# insights
+# The average basket size indicates that customers are purchasing a moderate number of items per order, which
