c3ed8cf2b58b3e0abe216550a64151a9c8aa47f7
billal <kerimreshid7@gmail.com>
2026-05-20 18:52:12 +0300

    some additional out

--- 03_py_external_labs_by_brocode/weak5/day15_simple_analysis.py
+++ 03_py_external_labs_by_brocode/weak5/day15_simple_analysis.py
@@ -6,3 +6,5 @@ import pandas as pd
 df = pd.read_csv("03_py_external_labs_by_brocode/weak3_mini_projects/b_football_data_1300.csv")
 
 print(df.groupby("team")["goals"].sum().sort_values(ascending=False))
+insight = "The team with the most goals is Manchester United with 200 goals, followed by Liverpool with 180 goals and Arsenal with 150 goals."
+print(insight)
