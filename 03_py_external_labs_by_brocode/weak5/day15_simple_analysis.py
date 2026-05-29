72930ea14c0136a22f7832e31c036013b8c301ef
billal <kerimreshid7@gmail.com>
2026-05-29 21:38:00 +0300

    Revision on day 15.

--- 03_py_external_labs_by_brocode/weak5/day15_simple_analysis.py
+++ 03_py_external_labs_by_brocode/weak5/day15_simple_analysis.py
@@ -8,3 +8,6 @@ df = pd.read_csv("03_py_external_labs_by_brocode/weak3_mini_projects/b_football_
 print(df.groupby("team")["goals"].sum().sort_values(ascending=False))
 insight = "The team with the most goals is Manchester United with 200 goals, followed by Liverpool with 180 goals and Arsenal with 150 goals."
 print(insight)
+
+# insights
+# The total goals by team provides insights into the offensive performance of each team in the league.
