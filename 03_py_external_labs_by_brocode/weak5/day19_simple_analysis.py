fea49a59e44bf14605958181b6723eec74bc9b1e
billal <kerimreshid7@gmail.com>
2026-05-31 06:27:40 +0300

    Revision on day 19.

--- 03_py_external_labs_by_brocode/weak5/day19_simple_analysis.py
+++ 03_py_external_labs_by_brocode/weak5/day19_simple_analysis.py
@@ -12,5 +12,6 @@ df["price_segment"] = pd.cut(df["price"], bins=bins, labels=labels)
 
 print(df.groupby("price_segment")["revenue"].sum())
 
-
+# insight: 
+# 1. The Premium price segment generates the highest total revenue, followed by the High and Medium segments. This indicates that customers are willing to spend more on higher-priced products, which may suggest a preference for premium offerings. The Low price segment generates the least revenue, which could indicate that customers are less interested in lower-priced products or that there may be a need to reevaluate the pricing strategy for this segment to increase its appeal and revenue potential.
 
