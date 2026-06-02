00a5b35eec493ca3e08d1e92faa3c89d8a99e61c
billal <kerimreshid7@gmail.com>
2026-06-02 21:23:52 +0300

    I am practicing matplotlip.

--- 03_py_external_labs_by_brocode/weak6/day20_data_visualization.py
+++ 03_py_external_labs_by_brocode/weak6/day20_data_visualization.py
@@ -1,3 +1,35 @@
+# hello everyone, in this video we will be learning about data visualization using matplotlib in python. Data visualization is the graphical representation of information and data. It helps us to understand complex data by presenting it in a visual format. Matplotlib is a popular library in python for creating static, animated, and interactive visualizations. 
+# To get started, we need to install matplotlib if we haven't already. You can do this using pip
 
 
-import matplotlip
+import matplotlib.pyplot as plt
+print(plt.matplotlib.__version__)
+
+# line chart
+# Used for trends over time.
+
+import matplotlib.pyplot as plt
+
+months = ["Jan", "Feb", "Mar", "Apr"]
+sales = [1200, 1500, 1800, 2200]
+
+plt.plot(months, sales)
+plt.title("Monthly Sales")
+plt.xlabel("Month")
+plt.ylabel("Sales")
+plt.show()
+
+
+# 2. Bar Chart
+# Used to compare categories.
+
+import matplotlib.pyplot as plt
+
+products = ["A", "B", "C"]
+sales = [300, 500, 200]
+
+plt.bar(products, sales)
+plt.title("Product Sales")
+plt.xlabel("Product")
+plt.ylabel("Sales")
+plt.show()
